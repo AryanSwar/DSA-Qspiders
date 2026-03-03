ea67a923d4bc860f8af44bd406be897f589d43d3
AryanSwar <aryans5678@gmail.com>
2026-03-03 23:35:04 +0530

    24th commit

--- BasicProgramming/src/Patterns/Pattern28TriangleFibonacci.java
+++ BasicProgramming/src/Patterns/Pattern28TriangleFibonacci.java
@@ -1,6 +1,12 @@
 package Patterns;
 
 public class Pattern28TriangleFibonacci{
+	/*
+	 * 						0 
+							1 1 
+							2 3 5 
+							8 13 21 34 
+	 */
 	public static void main(String[] args) {
 		int n1=0;
 		int n2=1;
