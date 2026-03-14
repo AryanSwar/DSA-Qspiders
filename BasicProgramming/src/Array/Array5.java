ac743b4ecb8b9134351f65890224167a7cb4aee1
AryanSwar <aryans5678@gmail.com>
2026-03-15 00:10:23 +0530

    29th commit

--- BasicProgramming/src/Array/Array5.java
+++ BasicProgramming/src/Array/Array5.java
@@ -7,7 +7,7 @@ public class Array5 {
 	public static void reverseArray(int arr[]) {
 		int i = 0;
 		int j = arr.length-1;
-		while(i<=j) {
+		while(i<j) {
 			int temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
