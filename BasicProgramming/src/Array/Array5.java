5dc17ac238a4fca4184e71d14a677e223dc63dfa
AryanSwar <aryans5678@gmail.com>
2026-03-15 00:07:18 +0530

    28th commit

--- BasicProgramming/src/Array/Array5.java
+++ BasicProgramming/src/Array/Array5.java
@@ -2,7 +2,9 @@ package Array;
 
 public class Array5 {
 	//Reverse an Array
-	public static void swapArray(int arr[]) {
+	//time complexity O(n)
+	//space complexity O(1)
+	public static void reverseArray(int arr[]) {
 		int i = 0;
 		int j = arr.length-1;
 		while(i<=j) {
@@ -16,7 +18,7 @@ public class Array5 {
 
 	public static void main(String[] args) {
 		int arr[] = {2,4,6,8,10,12};
-		swapArray(arr);
+		reverseArray(arr);
 		for(int x : arr) {
 			System.out.print(x+" ");
 		}
