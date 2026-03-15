3556248333ef760e025ca0dc74ffbe49579b3f05
AryanSwar <aryans5678@gmail.com>
2026-03-15 13:41:31 +0530

    30th commit

---
+++ BasicProgramming/src/Array/Array6.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Array6 {
+	//Pairs in an Array
+	public static void printPairs(int arr[]) {
+		int tp = 0; //total pair
+		for(int i=0;i<arr.length-1;i++) {
+			int count = 0;
+			for(int j=i+1;j<arr.length;j++) {
+				System.out.print("("+arr[i]+","+arr[j]+") ");
+				count++;
+				tp++;
+			}
+			System.out.print("pair count is : "+count);
+			System.out.println();
+		}
+		System.out.println("total no of pair's are : "+tp);
+	}
+	public static void main(String[] args) {
+		int arr[] = {2,4,6,8,10};
+		printPairs(arr);
+	}
+}
