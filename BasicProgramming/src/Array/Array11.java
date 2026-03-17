ad0f8fef1dbb8bb50cf3c8eeeea3fd8af6f8a612
AryanSwar <aryans5678@gmail.com>
2026-03-17 21:39:24 +0530

    34th commit

--- BasicProgramming/src/Array/Array11.java
+++ BasicProgramming/src/Array/Array11.java
@@ -2,12 +2,7 @@ package Array;
 
 public class Array11 {
 	public static void main(String[] args) {
-		int a = 10;
-		try {
-		int mul = 10/0;
-		}catch(ArithmeticException e) {
-			e.printStackTrace();
-		}
 		
 	}
 }
+
